21d383de6f38b37a023d4514e592ed0ccac32550
kathy95917 <kathy95917@naver.com>
2020-08-01 23:52:49 +0900

    Update containers

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ const api = axios.create({
 export const moviesApi = {
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
-  popular: () => api.get("movie/top_popular"),
+  popular: () => api.get("movie/popular"),
   movieDetail: (id) =>
     api.get(`movie/${id}`, {
       params: {
